64fda3023ca02ae7604e01f0b66788b25d3151b9
anshbhadauriya <2k23.csai2310627@gmail.com>
2026-03-06 00:42:21 +0530

    student JOin

--- app/src/main/java/com/example/attendx/Home/JoinClassActivity.kt
+++ app/src/main/java/com/example/attendx/Home/JoinClassActivity.kt
@@ -3,15 +3,19 @@ package com.example.attendx.Home
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.attendx.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class JoinClassActivity : AppCompatActivity() {
     private lateinit var class_code : EditText
     private lateinit var join_button : Button
+
+    private val db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +37,42 @@ class JoinClassActivity : AppCompatActivity() {
     private fun joinClass() {
         join_button.setOnClickListener {
             val code = class_code.text.toString()
-            //TODO kal krna haiii
+
+            if(code.isEmpty()){
+                Toast.makeText(this,"Enter class code",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            db.collection("classes")
+                .document(code)
+                .get()
+                .addOnSuccessListener { document ->
+
+                    if(document.exists()){
+
+                        val studentData = hashMapOf(
+                            "name" to "StudentName",
+                            "joinedAt" to System.currentTimeMillis()
+                        )
+                        db.collection("classes")
+                            .document(code)
+                            .collection("students")
+                            .add(studentData)
+
+                        Toast.makeText(this,"Class Found! Joined Successfully",Toast.LENGTH_SHORT).show()
+
+                    } else {
+
+                        Toast.makeText(this,"Invalid Class Code",Toast.LENGTH_SHORT).show()
+
+                    }
+
+                }
+                .addOnFailureListener {
+
+                    Toast.makeText(this,"Error connecting to server",Toast.LENGTH_SHORT).show()
+
+                }
+
         }
     }
 
